d970bdbcbd5a17301016d9d430dd7b2fa84e8893
Kimchheng <kimchhengforwork@gmail.com>
2026-01-14 15:46:38 +1100

    Initial Commit

---
+++ PersonalBlog/PersonalBlog/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace PersonalBlog.Areas.Admin
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View("New");
+        }
+
+        [HttpPost]
+        public IActionResult New(ArticleVm article) 
+        {
+            string taskFilePath = Path.Combine(AppContext.BaseDirectory + "article.txt");
+            string json = JsonSerializer.Serialize(article);
+            System.IO.File.WriteAllText(taskFilePath, json);
+            return RedirectToAction("New");
+        }
+    }
+}
---
+++ PersonalBlog/PersonalBlog/Areas/Admin/Models/ViewModels/Dashboard/ArticleVm.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalBlog.Areas.Admin
+{
+    public class ArticleVm
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+    }
+}
