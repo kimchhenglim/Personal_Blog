933b5ca7d9a94e688eabaf7cd8e7b5cc2d6d8bc9
Kimchheng <kimchhengforwork@gmail.com>
2026-01-16 20:21:41 +1100

    Add function Add New, and Delete

--- PersonalBlog/PersonalBlog/Areas/Admin/Controllers/DashboardController.cs
+++ PersonalBlog/PersonalBlog/Areas/Admin/Controllers/DashboardController.cs
@@ -8,22 +8,56 @@ namespace PersonalBlog.Areas.Admin
     {
         public IActionResult Index()
         {
-            return View();
+            List<ArticleVm> articles = new List<ArticleVm>();
+            string taskFilePath = Path.Combine(AppContext.BaseDirectory + "article.txt");
+            if(!System.IO.File.Exists(taskFilePath))
+                System.IO.File.Create(taskFilePath).Close();
+            string data = System.IO.File.ReadAllText(taskFilePath);
+            if (!string.IsNullOrEmpty(data))
+                articles.AddRange(JsonSerializer.Deserialize<List<ArticleVm>>(data)!);
+            
+            return View(articles);
         }
 
         [HttpGet]
         public IActionResult New()
         {
-            return View("New");
+            return View("New", new ArticleVm());
         }
 
         [HttpPost]
         public IActionResult New(ArticleVm article) 
         {
+            List<ArticleVm> articles = new List<ArticleVm>();
             string taskFilePath = Path.Combine(AppContext.BaseDirectory + "article.txt");
-            string json = JsonSerializer.Serialize(article);
+            string data = System.IO.File.ReadAllText(taskFilePath);
+            if (!string.IsNullOrEmpty(data))
+                articles.AddRange(JsonSerializer.Deserialize<List<ArticleVm>>(data)!);
+            int setupID = articles.Count > 0 ? articles.Max(x => x.Id) + 1 : 1;
+            article.Id = setupID;
+            articles.Add(article);
+            string json = JsonSerializer.Serialize(articles);
             System.IO.File.WriteAllText(taskFilePath, json);
-            return RedirectToAction("New");
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            List<ArticleVm> articles = new List<ArticleVm>();
+            string taskFilePath = Path.Combine(AppContext.BaseDirectory + "article.txt");
+            string data = System.IO.File.ReadAllText(taskFilePath);
+            if (!string.IsNullOrEmpty(data))
+                articles.AddRange(JsonSerializer.Deserialize<List<ArticleVm>>(data)!);
+
+            ArticleVm? article = articles.FirstOrDefault(x => x.Id == id);
+            if (article == null) return NotFound();
+
+            articles.Remove(article);
+            string json = JsonSerializer.Serialize(articles);
+            System.IO.File.WriteAllText(taskFilePath, json);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- PersonalBlog/PersonalBlog/Areas/Admin/Models/ViewModels/Dashboard/ArticleVm.cs
+++ PersonalBlog/PersonalBlog/Areas/Admin/Models/ViewModels/Dashboard/ArticleVm.cs
@@ -2,8 +2,9 @@
 {
     public class ArticleVm
     {
-        public string Title { get; set; }
-        public DateTime Date { get; set; }
-        public string Content { get; set; }
+        public int Id { get; set; } = 0;
+        public string Title { get; set; } = "";
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Content { get; set; } = "";
     }
 }
