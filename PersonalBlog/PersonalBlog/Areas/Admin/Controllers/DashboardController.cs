2f7f8dc04dd7456d9573fca470ebd142c1ec52de
Kimchheng <kimchhengforwork@gmail.com>
2026-01-22 17:12:30 +1100

    Add Edit Function

--- PersonalBlog/PersonalBlog/Areas/Admin/Controllers/DashboardController.cs
+++ PersonalBlog/PersonalBlog/Areas/Admin/Controllers/DashboardController.cs
@@ -22,7 +22,7 @@ namespace PersonalBlog.Areas.Admin
         [HttpGet]
         public IActionResult New()
         {
-            return View("New", new ArticleVm());
+            return View(new ArticleVm());
         }
 
         [HttpPost]
@@ -38,7 +38,46 @@ namespace PersonalBlog.Areas.Admin
             articles.Add(article);
             string json = JsonSerializer.Serialize(articles);
             System.IO.File.WriteAllText(taskFilePath, json);
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            List<ArticleVm> articles = new List<ArticleVm>();
+            string taskFilePath = Path.Combine(AppContext.BaseDirectory + "article.txt");
+            if (!System.IO.File.Exists(taskFilePath))
+                System.IO.File.Create(taskFilePath).Close();
+            string data = System.IO.File.ReadAllText(taskFilePath);
+            if (!string.IsNullOrEmpty(data))
+                articles.AddRange(JsonSerializer.Deserialize<List<ArticleVm>>(data)!);
+
+            ArticleVm? article = articles.FirstOrDefault(x => x.Id == id);
+            if (article == null) return NotFound();
+
+            return View(article);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ArticleVm model)
+        {
+            List<ArticleVm> articles = new List<ArticleVm>();
+            string taskFilePath = Path.Combine(AppContext.BaseDirectory + "article.txt");
+            string data = System.IO.File.ReadAllText(taskFilePath);
+            if (!string.IsNullOrEmpty(data))
+                articles.AddRange(JsonSerializer.Deserialize<List<ArticleVm>>(data)!);
+
+            ArticleVm? article = articles.FirstOrDefault(x => x.Id == model.Id);
+            if (article == null) return NotFound();
+
+            article.Title = model.Title;
+            article.Date = model.Date;
+            article.Content = model.Content;
+
+            string json = JsonSerializer.Serialize(articles);
+            System.IO.File.WriteAllText(taskFilePath, json);
+
+            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
